5cdc43b9a3b7c22a97bc532e2fe737151d8d35ad
yangty152 <35477773+yangty152@users.noreply.github.com>
2021-07-20 21:59:08 -0400

    updated app.py

--- app.py
+++ app.py
@@ -21,38 +21,41 @@ connection = engine.connect()
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/index.html')
 def welcome():
     return render_template('index.html')
-@app.route("/about_us.html")
+@app.route('/about_us.html')
 def aboutus():
     return render_template('about_us.html')
+@app.route('/job_cloud.html')
+def job_cloud():
+    return render_template('job_cloud.html')
 #Tianyue added interactive map route
-@app.route("/interactive_map.html")
+@app.route('/interactive_map.html')
 def interactive_map():
     return render_template('interactive_map.html')
 
-@app.route("/housing")
+@app.route('/housing')
 def housing():
     # Create session (link) from Python to the DB
     housing_df = pd.read_sql("SELECT * FROM housing", connection)
     housing_json = housing_df.to_json(orient = "records")
     return housing_json
 
-@app.route("/ds_jobs")
+@app.route('/ds_jobs')
 def ds_jobs():
     ds_jobs_df = pd.read_sql("SELECT * FROM ds_jobs", connection)
     ds_jobs_df.drop(columns = ["salary_offered"], inplace = True)
     ds_jobs_json = ds_jobs_df.to_json(orient = "records")
     return ds_jobs_json
 
-@app.route("/breweries")
+@app.route('/breweries')
 def breweries():
     breweries_df = pd.read_sql("SELECT * FROM breweries", connection)
     breweries_json = breweries_df.to_json(orient = "records")
     return breweries_json
 
-@app.route("/salary")
+@app.route('/salary')
 def salary():
     salary_df = pd.read_sql("SELECT * FROM salary", connection)
     salary_df.replace("Database Administrators and Architects", "Data Engineer", inplace = True)
@@ -60,7 +63,7 @@ def salary():
     salary_json = salary_df.to_json(orient = "records")
     return salary_json
 
-@app.route("/state_boundary")
+@app.route('/state_boundary')
 def state_boundary():
     state_boundary_df = pd.read_sql("SELECT * FROM state_boundary", connection)
     state_boundary_json = state_boundary_df.to_json(orient = "records")
